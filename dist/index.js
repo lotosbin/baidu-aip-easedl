667b62dbd1cd75eeaf32903da5fcbfccef2411ed
liubinbin <lotosbin@gmail.com>
2020-07-12 15:48:14 +0800

    build: v1.0.2

--- dist/index.js
+++ dist/index.js
@@ -39,6 +39,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Client = void 0;
 var moment_1 = __importDefault(require("moment"));
 var node_fetch_1 = __importDefault(require("node-fetch"));
 var utf8_1 = __importDefault(require("utf8"));
