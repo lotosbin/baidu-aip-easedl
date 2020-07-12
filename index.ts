7a25dc18a79cb974f7947c47e9bb59ab5eef5602
liubinbin <lotosbin@gmail.com>
2020-07-12 15:37:44 +0800

    feat: add label_list & label_delete method

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
+import moment from "moment";
 import fetch from "node-fetch";
 import utf8 from "utf8";
 
 interface TCache {
-    access_token: null | string,
-    dataset: null | Array<TDataSetListItem>
+    access_token: null | string;
+    dataset: null | TDataSetListItem[];
 }
 
-const cache: TCache = {access_token: null, dataset: null};
-
 export interface TDataSetListResult {
-    results: Array<TDataSetListItem>;
+    results: TDataSetListItem[];
 }
 
 export interface TDataSetListItem {
@@ -22,132 +21,127 @@ export interface TPriorityResult {
     score: number;
 }
 
-export interface TResult<T> {
-    error_code: number,
-    error_msg: string,
-    log_id: number,
-    result: T,
+export interface TResults<T> {
+    error_code: number;
+    error_msg: string;
+    log_id: number;
+    results: T;
 }
 
-export interface TResults<T> {
-    error_code: number,
-    error_msg: string,
-    log_id: number,
-    results: T,
+export interface TLabel {
+    label_id: String;
+    label_name: String;
+    entity_count: Number;
 }
 
 export type DataSetType = "IMAGE_CLASSIFICATION" | "OBJECT_DETECTION" | "IMAGE_SEGMENTATION" | "SOUND_CLASSIFICATION" | "TEXT_CLASSIFICATION" | String;
 
-export class Client {
-    private api_key: String;
-    private secret_key: String;
-    protected url: string;
-    protected text_cls_endpoint: String;
+export interface ClientOptions {
+    text_cls_endpoint: string;
+}
 
-    constructor(api_key: String, secret_key: String, {text_cls_endpoint}: any) {
-        this.api_key = api_key;
-        this.secret_key = secret_key;
+export class Client {
+    protected url: string;
+    protected cache: TCache = {access_token: null, dataset: null};
+    // tslint:disable-next-line:variable-name
+    protected text_cls_endpoint = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/text_cls/recommend_priority";
+
+    /**
+     * @param api_key
+     * @param secret_key
+     * @param options
+     */
+    // tslint:disable-next-line:variable-name
+    constructor(private api_key: String, private secret_key: String, private options: ClientOptions) {
         this.url = `https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=${api_key}&client_secret=${secret_key}`;
-        this.text_cls_endpoint = text_cls_endpoint;
+        console.log(`api_key=${api_key},secret_key=${secret_key}`);
     }
 
-
     protected async getAccessToken() {
-        if (cache.access_token) {
-            return cache.access_token;
+        if (this.cache.access_token) {
+            return this.cache.access_token;
         }
-        const response = await fetch(this.url, {method: 'POST'});
+        const response = await fetch(this.url, {method: "POST"});
         if (!response.ok) {
-            throw new Error(`${response.status}:${await response.text()}`)
+            throw new Error(`${response.status}:${await response.text()}`);
         }
         const json = await response.json();
         if (json.error) {
-            throw new Error(`${json.error}:${json.error_description}`)
+            throw new Error(`${json.error}:${json.error_description}`);
         }
-        cache.access_token = json.access_token;
+        this.cache.access_token = json.access_token;
         return json.access_token;
     }
-
 
     /**
      * @see https://ai.baidu.com/docs#/EasyDL_TEXT_API/top
      * @return
      */
-    async text_cls_top(text: string): Promise<TResults<Array<TPriorityResult>>> {
-        const accessToken = await this.getAccessToken();
-        const url = `${this.text_cls_endpoint}?access_token=${accessToken}`;
+    public async text_cls_top(text: string): Promise<TResults<TPriorityResult[]>> {
+        const url = `${(this.options || {}).text_cls_endpoint || this.text_cls_endpoint}?access_token=${(await this.getAccessToken())}`;
         const response = await fetch(url, {
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify({
-                "text": `${text || "empty"}`,
-                "top_num": 6
+                text: `${text || "empty"}`,
+                top_num: 6
             })
         });
         if (!response.ok) {
-            throw new Error(`${response.status}:${await response.text()}`)
-        }
-        return await response.json();
-    }
-
-    async dataset_add_entity(type: DataSetType, datasetId: number, name: string, content: string, label: string) {
-        console.debug(`dataset_add_entity:name=${name},content=${content}`);
-        const accessToken = await this.getAccessToken();
-        const url = `https://aip.baidubce.com/rpc/2.0/easydl/dataset/addentity?access_token=${accessToken}`;
-        const response = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({
-                "type": type,
-                "dataset_id": datasetId,
-                "entity_content": `${utf8.encode(content.slice(0, 10000 / 2))}`,
-                "entity_name": name,
-                "labels": [{"label_name": `${label}`}]
-            })
-        });
-        if (!response.ok) {
-            throw new Error(`${response.status}:${await response.text()}`)
+            throw new Error(`${response.status}:${await response.text()}`);
         }
         const json = await response.json();
-        /*{
-        log_id: 8538749507596601000,
-        results: [
-         { name: '0', score: 0.7447099685668945 },
-         { name: '1', score: 0.2552900016307831 }
-        ]
-        }*/
-        console.debug(`dataset_add_entity:result=${JSON.stringify(json)}`);
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
         return json;
     }
 
-    async dataset_create(type: DataSetType, name: string): Promise<TDataSetListItem> {
-        console.debug(`dataset_list`);
-        const accessToken = await this.getAccessToken();
-        const url = `https://aip.baidubce.com/rpc/2.0/easydl/dataset/create?access_token=${accessToken}`;
+    /**
+     * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/0e4e34*
+     */
+    public async dataset_add_entity(type: DataSetType, datasetId: number, name: string, content: string, label: string): Promise<TResults<any>> {
+        console.debug(`dataset_add_entity:datasetId=${datasetId},label=${label},name=${name},content=${content}`);
+        const url = `https://aip.baidubce.com/rpc/2.0/easydl/dataset/addentity?access_token=${(await this.getAccessToken())}`;
         const response = await fetch(url, {
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify({
-                "type": type,
-                "dataset_name": `${name}`,
+                type,
+                dataset_id: datasetId,
+                entity_content: `${utf8.encode(content.slice(0, 10000 / 2))}`,
+                entity_name: name,
+                labels: [{label_name: `${label}`}]
             })
         });
         if (!response.ok) {
-            throw new Error(`${response.status}:${await response.text()}`)
+            throw new Error(`${response.status}:${await response.text()}`);
         }
         const json = await response.json();
-        /*{
-       { total_num: 2,
-      results:
-       [ { dataset_id: 33883,
-           dataset_name: 'auto',
-           type: 'TEXT_CLASSIFICATION',
-           status: 'normal' },
-         { dataset_id: 34015,
-           dataset_name: '1',
-           type: 'TEXT_CLASSIFICATION',
-           status: 'normal' } ],
-      log_id: 565872871 }
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
+        return json;
+    }
 
-        */
-        console.debug(`dataset_create:result=${JSON.stringify(json)}`);
+    /**
+     * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/5f0139f6
+     */
+    public async dataset_create(type: DataSetType, name: string): Promise<TDataSetListItem> {
+        console.debug(`dataset_list`);
+        const url = `https://aip.baidubce.com/rpc/2.0/easydl/dataset/create?access_token=${(await this.getAccessToken())}`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                type,
+                dataset_name: `${name}`,
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`${response.status}:${await response.text()}`);
+        }
+        const json = await response.json();
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
         return {
             dataset_id: json.dataset_id,
             dataset_name: name
@@ -155,41 +149,145 @@ export class Client {
     }
 
     /**
-     * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/b917e3e2
-     * */
-    async dataset_list(type: DataSetType, skip = 0, take = 100): Promise<TDataSetListResult> {
+     * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/d0f6dfe6
+     */
+    // tslint:disable-next-line:variable-name
+    public async dataset_delete(type: DataSetType, dataset_id: number): Promise<TResults<any>> {
+        console.debug(`dataset_list`);
+        const url = `https://aip.baidubce.com/rpc/2.0/easydl/dataset/delete?access_token=${(await this.getAccessToken())}`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                type,
+                dataset_id,
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`${response.status}:${await response.text()}`);
+        }
+        const json = await response.json();
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
+        return json;
+    }
+
+    /**
+     * * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/b917e3e2*
+     */
+    public async dataset_list(type: DataSetType, skip = 0, take = 100): Promise<TDataSetListResult> {
         console.debug(`dataset_list`);
         const accessToken = await this.getAccessToken();
         const url = `https://aip.baidubce.com/rpc/2.0/easydl/dataset/list?access_token=${accessToken}`;
         const response = await fetch(url, {
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify({
-                "type": type,
-                "start": skip,
-                "num": take,
+                type,
+                start: skip,
+                num: take,
             })
         });
         if (!response.ok) {
-            throw new Error(`${response.status}:${await response.text()}`)
+            throw new Error(`${response.status}:${await response.text()}`);
         }
         const json = await response.json();
-        /*{
-       { total_num: 2,
-      results:
-       [ { dataset_id: 33883,
-           dataset_name: 'auto',
-           type: 'TEXT_CLASSIFICATION',
-           status: 'normal' },
-         { dataset_id: 34015,
-           dataset_name: '1',
-           type: 'TEXT_CLASSIFICATION',
-           status: 'normal' } ],
-      log_id: 565872871 }
-
-        */
-        console.debug(`dataset_list:result=${JSON.stringify(json)}`);
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
         return json;
     }
 
+    /**
+     * @return
+     */
+    public async recommend_priority(text: string): Promise<TResults<TPriorityResult[]>> {
+        const accessToken = await this.getAccessToken();
+        const url = `${(this.options || {}).text_cls_endpoint || this.text_cls_endpoint}?access_token=${accessToken}`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                text: `${text || "empty"}`,
+                top_num: 6
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`${response.status}:${await response.text()}`);
+        }
 
+        const json = await response.json();
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
+        return json;
+    }
+
+    public async dataset_today(): Promise<TDataSetListItem> {
+        const date = moment(Date.now()).format("YYYYMMDD");
+        const list: TDataSetListItem[] = this.cache.dataset || (await this.dataset_list("TEXT_CLASSIFICATION") || {results: []}).results;
+        this.cache.dataset = list;
+        let dataset = list.find((it) => it.dataset_name === date || it.dataset_name === `${date} V1`);
+        if (!dataset) {
+            dataset = await this.dataset_create("TEXT_CLASSIFICATION", date);
+            this.cache.dataset = null;
+        }
+        return dataset;
+    }
+
+    /**
+     *
+     * @param type    是    string    数据集类型，可包括： IMAGE_CLASSIFICATION, OBJECT_DETECTION, IMAGE_SEGMENTATION, SOUND_CLASSIFICATION, TEXT_CLASSIFICATION分别对应：图像分类、物体检测、图像分割、声音分类、文本分类
+     * @param  dataset_id    是    number    数据集ID
+     * @param start    否    number    起始序号，默认0
+     * @param num    否    number    数量，默认20，最多100
+     * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/b50d8bad
+     */
+    // tslint:disable-next-line:variable-name
+    public async label_list(type: DataSetType, dataset_id: Number, start: Number = 0, num: Number = 20): Promise<TResults<TLabel[]>> {
+        const url = `https://aip.baidubce.com/rpc/2.0/easydl/label/list?access_token=${(await this.getAccessToken())}`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                type,
+                dataset_id,
+                start,
+                num,
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`${response.status}:${await response.text()}`);
+        }
+        const json = await response.json();
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
+        return json;
+    }
+
+    /**
+     *
+     * @param type    是    string    数据集类型，可包括： IMAGE_CLASSIFICATION, OBJECT_DETECTION, IMAGE_SEGMENTATION, SOUND_CLASSIFICATION, TEXT_CLASSIFICATION分别对应：图像分类、物体检测、图像分割、声音分类、文本分类
+     * @param  dataset_id    是    number    数据集ID
+     * @param label_name    是    string    标签/分类名称
+     * @see https://ai.baidu.com/docs#/EasyDL_DATA_API/28c205e4
+     */
+    // tslint:disable-next-line:variable-name
+    public async label_delete(type: DataSetType, dataset_id: Number, label_name: String): Promise<TResults<any>> {
+        const url = `https://aip.baidubce.com/rpc/2.0/easydl/label/delete?access_token=${(await this.getAccessToken())}`;
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                type,
+                dataset_id,
+                label_name,
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`${response.status}:${await response.text()}`);
+        }
+        const json = await response.json();
+        if (json.error_code) {
+            throw new Error(`${json.error_code}:${json.error_msg}`);
+        }
+        return json;
+    }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,37 +1,81 @@
-import {Client} from "../index";
+import {suite, test} from "@testdeck/mocha";
 import {expect} from "chai";
-import {suite, test} from '@testdeck/mocha';
+import moment from "moment";
+import {Client, TDataSetListItem} from "../index";
 
 @suite
-class BaiduApiEaseDLClientTest extends Client {
+class ClientTest extends Client {
+    private dataset_name: string = "";
+    private dataset_id: number = 0;
+
     constructor() {
-        const {BAIDU_API_EASEDL_SECRET_KEY, BAIDU_API_EASEDL_APP_KEY}: any = process.env;
-        super(BAIDU_API_EASEDL_APP_KEY, BAIDU_API_EASEDL_SECRET_KEY, {
+        super(process.env.BAIDU_AIP_EASEDL_API_KEY || "", process.env.BAIDU_AIP_EASEDL_SECRET_KEY || "", {
             text_cls_endpoint: "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/text_cls/recommend_priority",
         });
+        this.dataset_name = `test_${moment().format("YYYYMMDD")}`;
     }
 
-    @test getAccessToken_should_success() {
-        const accessToken = this.getAccessToken();
+    @test
+    public async getAccessToken_should_success() {
+        const accessToken = await this.getAccessToken();
         expect(!!accessToken);
     }
 
     @test
-    async text_cls_top_should_correct() {
-        let result = await this.text_cls_top("大家好，一名前端工程师。\n" +
+    public async text_cls_top_should_correct() {
+        const result = await this.text_cls_top("大家好，一名前端工程师。\n" +
             "人可以累，心不能累\n" +
             "刚入职时，作为一名技术新人，负责 PC 版的淘宝首页，这块业务很特殊，它的受众很多，每天都有上亿的流量，系统的复杂度虽不是很高，但是风险特别大，而且需要与很多很多很多人交涉，我接手之时业务发生了一些变化，作业量很大，压力也很大，有来自业务方的压力，也有技术上的挑战，那段时间，连续一两个月，加班到晚上 1 点左右回家，而且 1 点以后还有可能收到业务方的电话。\n" +
             "有一天晚上，优化一个技术细节问题，日的疲惫感让我一下子爆发了出来，那天晚上，我哽咽了，哭了，最后泣不成声，主管在旁边，他话不多，等我哭的差不多的时候，他告诉我，“成长是很累，但是人可以累，心不能累”。");
         console.log(result);
-        expect(result.results.length > 0)
+        expect(result.results.length > 0);
     }
 
-
     @test
-    async text_cls_dataset_list_should_correct() {
-        let result = await this.dataset_list("TEXT_CLASSIFICATION");
+    public async text_cls_dataset_list_should_correct() {
+        const result = await this.dataset_list("TEXT_CLASSIFICATION");
         console.log(result);
         expect(result.results.length > 0);
     }
-}
 
+    @test
+    public async dataset_create_should_correct() {
+        // clean
+        const list = await this.dataset_list("TEXT_CLASSIFICATION", 0, 100);
+        const find = list.results.find((e) => e.dataset_name === this.dataset_name || e.dataset_name === `${this.dataset_name} V1`);
+        if (find) {
+            console.log(`clean `);
+            await this.dataset_delete("TEXT_CLASSIFICATION", find.dataset_id);
+        }
+        // begin
+        const item = await this.dataset_create("TEXT_CLASSIFICATION", this.dataset_name);
+        expect(item.dataset_name).to.equal(this.dataset_name);
+        expect(item.dataset_id).to.be.exist;
+        this.dataset_id = item.dataset_id;
+    }
+
+    @test
+    public async dataset_add_entity_should_correct() {
+        const today = await this.dataset_today();
+        const results = await this.dataset_add_entity("TEXT_CLASSIFICATION", today.dataset_id, `test_${new Date().getSeconds()}`, "孙俪卡斯蒂略疯狂世界孙俪开始的交流方式看得见", "1");
+        expect(results.error_code).to.be.undefined;
+    }
+
+    @test
+    public async label_list_should_correct() {
+        const result = await this.dataset_list("TEXT_CLASSIFICATION");
+        const datasetId = result.results[0].dataset_id;
+        const results = await this.label_list("TEXT_CLASSIFICATION", datasetId);
+        console.log(results);
+        expect(results.error_code).to.be.undefined;
+    }
+
+    @test
+    public async label_delete_should_correct() {
+        const today = await this.dataset_today();
+        const results = await this.label_delete("TEXT_CLASSIFICATION", today.dataset_id, "1");
+        expect(results.error_code).to.be.undefined;
+        expect(results.log_id).to.exist;
+    }
+
+}
